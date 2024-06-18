04928f300bf0b422e1b5f277bdcfad0aa86bb3a3
FlavioOnGitHub <flaviorotunno@gmail.com>
2024-06-18 16:58:52 +0200

    starting to animate the "valeurs" section, the pin is now functional

--- src/js/valeurs.js
+++ src/js/valeurs.js
@@ -4,57 +4,52 @@ gsap.registerPlugin(ScrollTrigger);
 
 var valeurs = document.querySelector(".valeurs");
 
-gsap.to(".valeurs-slide-1", {
-  scrollTrigger: {
-    trigger: ".cards-container",
-    start: "top-=2000 bottom",
-    end: "top bottom",
-    markers: true,
-    pin: ".valeurs-slide-1",
-    scrub: 1,
-  },
-});
-
-gsap.to(".valeurs-image-1", {
-  scrollTrigger: {
-    trigger: ".cards-container",
-    start: "top-=2000 bottom",
-    end: "top+=400 bottom",
-    markers: true,
-    pin: ".valeurs-image-1",
-    scrub: 1,
-  },
-});
-
-gsap.to(".valeurs-image-2", {
-  scrollTrigger: {
-    trigger: ".cards-container",
-    start: "top-=2000 bottom",
-    end: "top+=400 bottom",
-    markers: true,
-    pin: ".valeurs-image-2",
-    scrub: 1,
-  },
-});
-
-gsap.to(".valeurs-image-3", {
-  scrollTrigger: {
-    trigger: ".cards-container",
-    start: "top-=2000 bottom",
-    end: "top+=400 bottom",
-    markers: true,
-    pin: ".valeurs-image-3",
-    scrub: 1,
-  },
-});
-
 gsap.to(".valeurs-background", {
   scrollTrigger: {
     trigger: ".cards-container",
     start: "top-=2000 bottom",
     end: "top bottom",
-    markers: true,
+    // markers: true,
     pin: ".valeurs-background",
     scrub: 1,
   },
+});
+
+document.querySelectorAll(".valeurs-text").forEach((slide) => {
+  gsap.to(slide, {
+    scrollTrigger: {
+      trigger: ".cards-container",
+      start: "top-=2000 bottom",
+      end: "top+=50 bottom",
+      // markers: true,
+      pin: slide,
+      scrub: 1,
+    },
+  });
+});
+
+document.querySelectorAll(".valeurs-image").forEach((image) => {
+  gsap.to(image, {
+    scrollTrigger: {
+      trigger: ".cards-container",
+      start: "top-=2000 bottom",
+      end: "top+=50 bottom",
+      // markers: true,
+      pin: image,
+      scrub: 1,
+    },
+  });
+});
+
+gsap.to(".valeurs-slide-1", {
+  scrollTrigger: {
+    trigger: ".cards-container",
+    start: "top-=2000 bottom",
+    end: "top-=1800 bottom",
+    markers: true,
+    scrub: 1,
+  },
+  x: -1200,
+  opacity: 0,
+  duration: 1,
 });
